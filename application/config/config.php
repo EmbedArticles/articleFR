8ae5fc2d5d776eb71867d3e7968e5d55762ab01d
Glenn Prialde <articlefr@users.noreply.github.com>
2014-07-09 15:11:17 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,17 +1,17 @@
 <?php 
 
-$config['admin_email'] = 'admin@freereprintables.com'; // admin email to use in email notices
+$config['admin_email'] = 'admin@test.com'; // admin email to use in email notices
 
 $config['template'] = 'modular'; // template to use
-$config['base_url'] = 'http://freereprintables.com/sandbox/'; // Base URL including trailing slash (e.g. http://localhost/)
+$config['base_url'] = 'http://example.com/'; // Base URL including trailing slash (e.g. http://localhost/)
 
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
 
 $config['db_host'] = 'localhost'; // Database host (e.g. localhost)
-$config['db_name'] = 'freerep_articlefr'; // Database name
-$config['db_username'] = 'freerep_dbuser'; // Database username
-$config['db_password'] = '8d&g6~u6qCd&'; // Database password
+$config['db_name'] = 'test_db'; // Database name
+$config['db_username'] = 'test_user'; // Database username
+$config['db_password'] = 'test123'; // Database password
 
 $config['autoload_helpers'] = array('session_helper', 'url_helper');
 
